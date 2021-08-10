3849ceb2e5955db13c3a99768e6e00e4c5d92caa
bidhanroyjoy <bidhan19994@gmail.com>
2021-08-10 23:40:15 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -6,45 +6,52 @@ import Checkout from "./Checkout/Checkout";
 import Login from "./Login/Login";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
+import Payment from "./Payment/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51JMKHSIlcdKfMJJZrhkbGvNB6PMvKI0qCp6Sf3KE3N32oI1cx3CCDgUkXZKbM6OcQpHubBOttDbn1Yeu8CMSEWUn00e56ugKiK"
+);
 
 function App() {
+  const [{}, dispatch] = useStateValue();
 
-  const [{},dispatch]=useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The User Is >>>", authUser);
 
-  useEffect(
-    ()=>{
-      auth.onAuthStateChanged((authUser)=>{
-        console.log("The User Is >>>",authUser);
-
-        if(authUser) {
-          dispatch({
-            type:"SET_USER",
-            user:authUser,
-          });
-        } else {
-          dispatch({
-            type:"SET_USER",
-            user:null,
-          });
-        }
-      });
-    },[]
-  );
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
-      <Header/>
+        <Header />
         <Switch>
-
-        <Route path="/login">
-          <Login />
-        </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/Checkout">
-            <Checkout/>
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
-         
-            <Home /> 
+            <Home />
           </Route>
         </Switch>
       </div>
--- src/Subtotal/Subtotal.js
+++ src/Subtotal/Subtotal.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import {Link,useHistory} from "react-router-dom";
 import { useStateValue } from '../StateProvider';
 import { getBasketTotal } from '../reducer';
 
 function Subtotal() {
 
     const [{basket},dispatch] = useStateValue();
+    const history = useHistory();
+
     return (
         <div className="subtotal">
             <CurrencyFormat 
@@ -17,7 +20,7 @@ function Subtotal() {
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" /> This order contains a gift
-                        </small>
+                        </small> 
                     </>
                 )
                 }
@@ -28,10 +31,10 @@ function Subtotal() {
                 prefix={"$"}
             />
 
-            <button>Proceed to Checkout</button>
+            <button onClick={e=>history.push('/payment')}>Proceed to Checkout</button>
             
         </div>
-    )
+    );
 }
 
 export default Subtotal;
